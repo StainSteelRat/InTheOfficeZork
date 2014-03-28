b2ea7c06b009300e1a01e6589d5f87ef47a85d4b
StainSteelRat <justinstroud@gmail.com>
2014-03-28 17:55:29 -0400

    First load

---
+++ InTheOfficeZork/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InTheOffice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, cube slave. What is your name? Not that it matters.");
+            Console.Write(">");
+            string sResponse = Console.ReadLine();
+            Console.WriteLine("You, cube slave, are here for data entry. I know, I know. Your name is {0}. That doesn't matter. Just pay attention to my instructions.", sResponse);
+            Console.ReadLine();
+        }
+    }
+}
